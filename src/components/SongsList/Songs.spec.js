6593b27f5f6aacbbb0b26aa5c4c4e200ab8d3600
Melam Prathyusha <melamprathyusha@Melams-MacBook-Pro.local>
2024-07-15 21:21:59 +0530

    Add. Display songs and Add to playlist button to each song in main page

--- src/components/SongsList/Songs.spec.js
+++ src/components/SongsList/Songs.spec.js
@@ -1,2 +1,18 @@
 import React from "react";
+import SongsList from "./SongsList";
+import { render,screen } from "@testing-library/react";
 
+describe('Songs Rendering',()=>{
+   it ('should display songs in the main page',async ()=>{
+        render(<SongsList/>);
+    
+        const songsList = await screen.findAllByRole('listitem');
+    
+        expect(songsList).toHaveLength(4);
+
+   });
+
+  
+    
+
+});
--- src/components/SongsList/SongsList.js
+++ src/components/SongsList/SongsList.js
@@ -1,8 +1,33 @@
+import { Box, Button, List, ListItem, ListItemSecondaryAction, ListItemText } from "@mui/material";
 import React from "react";
+
+const allSongs = [
+        {id:1,title:"Song 1"},
+        {id:2,title:"Song 2"},
+        {id:3,title:"Song 3"},
+        {id:4,title:"Song 4"},
+];
 
 const SongsList = () =>{
     return(
-        <h1>SongsList</h1>
+        <Box style={{padding: 2,
+        borderRadius: 1,
+        maxWidth: '400px',
+        margin: '10 auto',}}>
+            <List>
+            {allSongs.map((song)=>(
+                <ListItem key={song.id}>
+                    <ListItemText primary={song.title} />
+                    <ListItemSecondaryAction>
+                        <Button variant="contained" color="primary">
+                            Add to Playlist
+                        </Button >
+                    </ListItemSecondaryAction>
+                </ListItem>
+            ))}
+        </List>
+        </Box>
+        
     );
 }
 
