ac319ed5872bdcd283e46ac651bd34052fdef431
Melam Prathyusha <melamprathyusha@Melams-MacBook-Pro.local>
2024-07-16 18:34:14 +0530

    Refactor. Songs in a seperate component

--- src/components/SongsList/SongsList.js
+++ src/components/SongsList/SongsList.js
@@ -1,6 +1,6 @@
-import { Box, Button, Dialog, DialogContent, DialogTitle, IconButton, List, ListItem, ListItemSecondaryAction, ListItemText } from "@mui/material";
-import React, { useState } from "react";
-import CloseIcon from '@mui/icons-material/Close';
+import { Box,  List } from "@mui/material";
+import React from "react";
+import Song from "./Song";
 
 const allSongs = [
     { id: 1, title: "Song 1" },
@@ -9,29 +9,7 @@ const allSongs = [
     { id: 4, title: "Song 4" },
 ];
 
-const SongsList = ({ playlists,onAddToPlaylist }) => {
-
-
-    const [open, setOpen] = useState(false);
-
-    const [selectedSong, setSelectedSong] = useState(null);
-
-    const handleDialogOpen =(song)=>{
-        setSelectedSong(song);
-        setOpen(true);
-    }
-
-    const handleDialogClose =()=>{
-        setOpen(false);
-        setSelectedSong(null);
-    }
-
-    const handleAddToPlaylist = (playlistId) => {
-        if (selectedSong) {
-            onAddToPlaylist(selectedSong.id, playlistId);
-            handleDialogClose();
-        }
-    };
+const SongsList = ({ playlists, onAddToPlaylist }) => {
 
     return (
         <Box style={{
@@ -42,38 +20,9 @@ const SongsList = ({ playlists,onAddToPlaylist }) => {
         }}>
             <List>
                 {allSongs.map((song) => (
-                    <ListItem key={song.id}>
-                        <ListItemText primary={song.title} />
-                        <ListItemSecondaryAction>
-                            <Button variant="contained" color="primary" onClick={()=>handleDialogOpen(song)} data-testid={`add-to-playlist-button-${song.id}`}>
-                                Add to Playlist
-                            </Button >
-                        </ListItemSecondaryAction>
-                    </ListItem>
+                    <Song song={song} playlists={playlists} onAddToPlaylist={onAddToPlaylist} />
                 ))}
             </List>
-
-            <Dialog open={open} maxWidth="sm" PaperProps={{style: {width: 500,},}} data-testid="addToPlayList-dialog">
-                <DialogTitle>
-                    Add to Playlist
-                    <IconButton onClick={()=>handleDialogClose()} edge="end" sx={{ position: 'absolute', right: 30, top: 8 }}>
-                        <CloseIcon/>
-                    </IconButton>
-
-                </DialogTitle>
-
-                <DialogContent sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        gap: 1,
-                    }}>
-                    {playlists.map((playlist) => (
-                        <Button variant="contained" color="primary" key={playlist.id} sx={{ mb: 1 }} onClick={() => handleAddToPlaylist(playlist.id)}>
-                            {playlist.name}
-                        </Button>
-                    ))}
-                </DialogContent>
-            </Dialog>
         </Box>
 
 
